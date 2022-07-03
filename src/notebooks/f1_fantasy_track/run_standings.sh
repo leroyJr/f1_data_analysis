79824db36102cd2f7951ae0e19e5cd16895478cc
Michael Matney <mikematneyjr@gmail.com>
2022-07-03 16:55:37 -0500

    Data+, shell script for standings
    * More sentiment data
    * Updated points and price data
    * Added shell script to run papermill execution of standings

---
+++ src/notebooks/f1_fantasy_track/run_standings.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+papermill 01_get_f1_fantasy_tracker.ipynb /tmp/01_x.ipynb
+papermill 02_transform_f1_fantasy_tracker.ipynb /tmp/02_x.ipynb
+papermill 03_analytics_f1_fantasy_tracker.ipynb /tmp/03_x.ipynb
